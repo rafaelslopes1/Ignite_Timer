701cd8bde69174646333469467214670411a0dff
Rafael Lopes <rafaelslopes159@gmail.com>
2023-04-08 12:38:47 -0300

    feat: configuração de tema e eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <h1>Ignite Timer</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
