14ad8b5b8f65f7e1ac4fe4c10e1fec42df78ca9e
Rafael Lopes <rafael.lopes@scora.com.br>
2023-07-19 19:43:51 -0300

    feat: Páginas Home e History parcialmente finalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
